ca9d366e99e0ebcfadaea3bbd0f4d4302296200e
Luke Burns <luke@lukeburns.com>
2017-05-17 10:45:41 -0400

    ignore directory touch bug

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ var touch = require('touch')
 var mkdirp = require('mkdirp')
 var p = require('path')
 
-module.exports = function (path, opts, callback) {
-  if (!callback && typeof opts === 'function') {
-    callback = opts
-    opts = {}
-  }
-
+module.exports = function (path, options, callback) {
+  if (typeof options === "function") cb = options, options = null
   var dir = (path[path.length-1] === '/') ? path : p.dirname(path)
-  mkdirp(dir, opts, function (merr) {
-    if (!merr && dir !== path) touch(path, opts, function (terr) {
-      callback(terr)
+  mkdirp(dir, options, function (er) {
+    if (!er && dir !== path) touch(path, options, function (err) {
+      if (err && err.code === 'EISDIR') err = undefined // awaiting PR to be merged: https://github.com/isaacs/node-touch/pull/14
+      callback(err)
     })
-    else callback(merr)
+    else callback(er)
   })
 }
